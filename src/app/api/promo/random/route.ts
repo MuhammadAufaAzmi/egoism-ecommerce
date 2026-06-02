f0eaa65656b7082adff2330e6636c4e0ef938bf3
Muhammad Aufa Azmi <aufakatanax12@gmail.com>
2026-06-02 16:03:03 +0700

    fix: prisma import error in promo api

--- src/app/api/promo/random/route.ts
+++ src/app/api/promo/random/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 
 export const dynamic = "force-dynamic"; // Ensure it's not cached so we get a random one each time
 
