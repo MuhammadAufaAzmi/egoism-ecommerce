250a6d8ddc4d2fbf9df75a2d2635400e083fd476
Muhammad Aufa Azmi <aufakatanax12@gmail.com>
2026-06-12 16:24:43 +0700

    fix: remove broken media print from Material Symbols font link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,9 +79,6 @@ export default function RootLayout({
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
-          media="print"
-          // @ts-expect-error
-          onLoad="this.media='all'"
         />
         <noscript>
           <link
