b8d22e4224363939ba4a994cd52e84e353331a61
Muhammad Aufa Azmi <aufakatanax12@gmail.com>
2026-07-07 21:30:34 +0700

    Fix typescript error in clear cache route

--- src/app/api/clear-cache/route.ts
+++ src/app/api/clear-cache/route.ts
@@ -1,10 +1,7 @@
-import { revalidateTag, revalidatePath } from 'next/cache';
+import { revalidatePath } from 'next/cache';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  revalidateTag('products');
-  revalidateTag('products-all');
-  revalidateTag('related-products');
   revalidatePath('/', 'layout');
   return NextResponse.json({ revalidated: true, message: "Cache completely wiped." });
 }
