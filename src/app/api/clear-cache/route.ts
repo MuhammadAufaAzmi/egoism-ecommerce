3f7875830179d6c32b869fb3de72c57fa2a0c7cc
Muhammad Aufa Azmi <aufakatanax12@gmail.com>
2026-07-07 21:45:22 +0700

    Fix cache route tags

--- src/app/api/clear-cache/route.ts
+++ src/app/api/clear-cache/route.ts
@@ -1,7 +1,11 @@
-import { revalidatePath } from 'next/cache';
+import { revalidatePath, revalidateTag } from 'next/cache';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
+  // @ts-ignore
+  revalidateTag('products');
+  // @ts-ignore
+  revalidateTag('products-all');
   revalidatePath('/', 'layout');
   return NextResponse.json({ revalidated: true, message: "Cache completely wiped." });
 }
