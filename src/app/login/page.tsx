12506411d1b849ae44f87d628bf3029e27432379
Muhammad Aufa Azmi <aufakatanax12@gmail.com>
2026-07-07 21:12:52 +0700

    Fix Google Login CORS error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -88,7 +88,7 @@ export default function LoginPage() {
           )}
 
           <div className="mb-6">
-            <Link
+            <a
               href="/api/auth/google"
               className="w-full border border-primary/30 flex items-center justify-center gap-3 py-4 text-[13px] font-semibold tracking-widest uppercase hover:bg-primary/5 transition-colors duration-300"
             >
@@ -99,7 +99,7 @@ export default function LoginPage() {
                 <path fill="currentColor" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" />
               </svg>
               LANJUTKAN DENGAN GOOGLE
-            </Link>
+            </a>
           </div>
           
           <div className="flex items-center gap-4 mb-6">
